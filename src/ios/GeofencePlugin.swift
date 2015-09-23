c9d19f8e8e292b9fe82d351df1b0d47783d98fe6
Rasita Satianrapapong <rasita_sa@hotmail.com>
2015-09-23 15:48:04 +0700

    [iOS] Support 20+ geofences and add 2x-radius filter upon entering region

--- src/ios/GeofencePlugin.swift
+++ src/ios/GeofencePlugin.swift
@@ -12,6 +12,12 @@ import AudioToolbox
 let TAG = "GeofencePlugin"
 let iOS8 = floor(NSFoundationVersionNumber) > floor(NSFoundationVersionNumber_iOS_7_1)
 let iOS7 = floor(NSFoundationVersionNumber) <= floor(NSFoundationVersionNumber_iOS_7_1)
+let monitoringLimit = 20
+let dummyRegionIdentifier = "DUMMY"
+var latitude = 0.0
+var longitude = 0.0
+var refreshing = false
+
 
 func log(message: String){
     NSLog("%@ - %@", TAG, message)
@@ -67,9 +73,19 @@ func log(message: String){
 
     func addOrUpdate(command: CDVInvokedUrlCommand) {
         dispatch_async(dispatch_get_global_queue(priority, 0)) {
-            // do some task
+            var overLimit = false
+            if self.geoNotificationManager.locationManager.monitoredRegions.count + command.arguments.count > monitoringLimit {
+                overLimit = true
+            }
             for geo in command.arguments {
-                self.geoNotificationManager.addOrUpdateGeoNotification(JSON(geo))
+                self.geoNotificationManager.addOrUpdateGeoNotification(JSON(geo),overLimit: overLimit)
+            }
+            log("Stored: \(self.geoNotificationManager.getWatchedGeoNotifications()?.count) Monitored: \(self.geoNotificationManager.locationManager.monitoredRegions.count)")
+            // If over the limit, get user location and let monitorNearbyRegions() handle the monitoring
+            if overLimit{
+                refreshing = true
+                self.geoNotificationManager.locationManager.startUpdatingLocation()
+                log("Number of total fences is over the limit. Start updating location")
             }
             dispatch_async(dispatch_get_main_queue()) {
                 let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
@@ -251,43 +267,52 @@ class GeoNotificationManager : NSObject, CLLocationManagerDelegate {
         } else {
             log("Location services enabled")
         }
-        if iOS8 {
-            locationManager.requestAlwaysAuthorization()
-        }
+//        if iOS8 {
+//            locationManager.requestAlwaysAuthorization()
+//        }
 
         if (!CLLocationManager.isMonitoringAvailableForClass(CLRegion)) {
             log("Geofencing not available")
         }
     }
 
-    func addOrUpdateGeoNotification(geoNotification: JSON) {
+    func addOrUpdateGeoNotification(geoNotification: JSON, overLimit: Bool) {
         log("GeoNotificationManager addOrUpdate")
 
         if (!CLLocationManager.locationServicesEnabled()) {
             log("Locationservices is not enabled")
         }
-
-        let location = CLLocationCoordinate2DMake(
-            geoNotification["latitude"].asDouble!,
-            geoNotification["longitude"].asDouble!
-        )
-        log("AddOrUpdate geo: \(geoNotification)")
-        let radius = geoNotification["radius"].asDouble! as CLLocationDistance
-        //let uuid = NSUUID().UUIDString
-        let id = geoNotification["id"].asString
-
-        let region = CLCircularRegion(center: location, radius: radius, identifier: id!)
         
-        var transitionType = 0
-        if let i = geoNotification["transitionType"].asInt {
-            transitionType = i
+        if iOS8 {
+            locationManager.requestAlwaysAuthorization()
         }
-        region.notifyOnEntry = 0 != transitionType & 1
-        region.notifyOnExit = 0 != transitionType & 2
-
+        
         //store
         store.addOrUpdate(geoNotification)
-        locationManager.startMonitoringForRegion(region)
+        
+        //If over the limit, postpone the monitoring until monitorNearbyRegions() is fired
+        if !overLimit{
+            log("Number of fences is under the limit. Start monitoring normally.")
+            let location = CLLocationCoordinate2DMake(
+                geoNotification["latitude"].asDouble!,
+                geoNotification["longitude"].asDouble!
+            )
+            log("AddOrUpdate geo: \(geoNotification)")
+            let radius = geoNotification["radius"].asDouble! as CLLocationDistance
+            //let uuid = NSUUID().UUIDString
+            let id = geoNotification["id"].asString
+            
+            let region = CLCircularRegion(center: location, radius: radius, identifier: id!)
+            
+            var transitionType = 0
+            if let i = geoNotification["transitionType"].asInt {
+                transitionType = i
+            }
+            region.notifyOnEntry = 0 != transitionType & 1
+            region.notifyOnExit = 0 != transitionType & 2
+            
+            locationManager.startMonitoringForRegion(region)
+        }
     }
 
     func getWatchedGeoNotifications() -> [JSON]? {
@@ -322,9 +347,99 @@ class GeoNotificationManager : NSObject, CLLocationManagerDelegate {
             locationManager.stopMonitoringForRegion(region)
         }
     }
-
+    
+    func distanceBetween(geo: JSON,location: CLLocation) -> Double? {
+        let origin = CLLocation(latitude: geo["latitude"].asDouble!, longitude: geo["longitude"].asDouble!)
+        let radius = geo["radius"].asDouble! as CLLocationDistance
+//        let name = geo["notification"]["title"]
+//        let id = geo["id"].asString!
+        let distanceToLocation = location.distanceFromLocation(origin) - radius
+        //log("distance to \(name) = \(location.distanceFromLocation(origin)) - \(radius) = \(distanceToLocation)")
+        return distanceToLocation
+    }
+    func monitorNearbyRegions(lat: Double, lon: Double){
+        log("Monitor nearby regions")
+        log("[Before refresh] Stored: \(getWatchedGeoNotifications()?.count) Monitored: \(locationManager.monitoredRegions.count)") //Expect (32,0) or (32,20)
+        //Stop all monitoring
+        for object in locationManager.monitoredRegions {
+            let region = object
+            log("Stoping monitoring region \(region.identifier)")
+            locationManager.stopMonitoringForRegion(region)
+        }
+        
+        log("[Between refresh] Stored: \(getWatchedGeoNotifications()?.count) Monitored: \(locationManager.monitoredRegions.count)") // Expect (32, 0)
+        let location = CLLocation(latitude: lat, longitude: lon)    //User location
+        var geos = getWatchedGeoNotifications()!    //All geofences stored
+        
+        // Sort by distance ascendingly
+        geos.sortInPlace{self.distanceBetween($0, location: location) < self.distanceBetween($1, location: location)}
+        log("Sorted distances")
+        
+        for geo in geos {
+            let id = geo["id"]
+            let name = geo["notification"]["title"]
+            log("ID \(id) \(name): \(distanceBetween(geo, location: location)!/1000) km")
+        }
+        
+        // Create and monitor dummy geofence (special geofence which covers the nearest [monitoringLimit - 1] geofences)
+        let farGeo = geos[monitoringLimit-1]
+        let farGeoOrigin = CLLocation(latitude: farGeo["latitude"].asDouble!,longitude: farGeo["longitude"].asDouble!)
+        var dummyRegionRadius = location.distanceFromLocation(farGeoOrigin)-(farGeo["radius"].asDouble!)    // from user location to the limit region's (i.e. 20th region) rim
+        // Prevent the radius from exceeding the limit
+        if (dummyRegionRadius > locationManager.maximumRegionMonitoringDistance) {
+            dummyRegionRadius = locationManager.maximumRegionMonitoringDistance;
+        }
+        let dummyRegion = CLCircularRegion(
+            center: CLLocationCoordinate2DMake(lat,lon),  // user location
+            radius: dummyRegionRadius,
+            identifier: dummyRegionIdentifier
+        )
+        // Notify on exit only
+        dummyRegion.notifyOnEntry = false
+        dummyRegion.notifyOnExit = true
+        log("Start monitoring dummy region")
+        locationManager.startMonitoringForRegion(dummyRegion)
+        
+        // Monitor the nearest [monitoringLimit - 1] geofences
+        var index : Int
+        for index = 0; index < monitoringLimit - 1; index++ {
+            let origin = CLLocationCoordinate2DMake(
+                geos[index]["latitude"].asDouble!,
+                geos[index]["longitude"].asDouble!
+            )
+            let region = CLCircularRegion(
+                center: origin,
+                radius: geos[index]["radius"].asDouble!,
+                identifier: geos[index]["id"].asString!
+            )
+            var transitionType = 0
+            if let i = geos[index]["transitionType"].asInt {
+                transitionType = i
+            }
+            region.notifyOnEntry = 0 != transitionType & 1
+            region.notifyOnExit = 0 != transitionType & 2
+            //region.notifyOnEntry = geos[index]["transitionType"].asInt == 1 ? true: false
+            //region.notifyOnExit = geos[index]["transitionType"].asInt == 2 ? true: false
+            log("Start monitoring region \(region.identifier)")
+            locationManager.startMonitoringForRegion(region)
+        }
+        
+        log("[After refresh] Stored: \(getWatchedGeoNotifications()?.count) Monitored: \(locationManager.monitoredRegions.count)")  //Expect (32, 20)
+    }
+    
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        log("update location")
+        if refreshing {
+            let locationArray = locations as NSArray
+            let locationObj = locationArray.lastObject as! CLLocation
+            let coord = locationObj.coordinate
+            latitude = coord.latitude
+            longitude = coord.longitude
+            log("New location = \(latitude), \(longitude)")
+            locationManager.stopUpdatingLocation()
+            log("Stop updating location")
+            refreshing = false
+            monitorNearbyRegions(latitude, lon: longitude)
+        }
     }
 
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
@@ -336,14 +451,52 @@ class GeoNotificationManager : NSObject, CLLocationManagerDelegate {
     }
 
     func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        log("Entering region \(region.identifier)")
-        handleTransition(region)
+        log("Entering region \(region.identifier) at location \(manager.location!.description)")
+        let location = manager.location!.coordinate // user location at the trigger time
+        let circularRegion = (region as! CLCircularRegion)  // convert to CLCircularRegion
+        // create circular region with doubled radius (in order not to make the filter too strict)
+        let coverageRegion = CLCircularRegion(center: circularRegion.center, radius: circularRegion.radius * 2, identifier: circularRegion.identifier)
+
+        if coverageRegion.containsCoordinate(location){
+            log("This event is reliable")
+            handleTransition(region, transitionType: "enter",location: location)
+        }
+        else{
+            log("This event is not reliable. Ignore it.")
+        }
     }
 
     func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
         log("Exiting region \(region.identifier)")
-        handleTransition(region)
-    }
+        var location = manager.location!.coordinate
+        
+        /*if !((region as CLCircularRegion).containsCoordinate(location)){
+        log("This event is reliable")
+        // If it's dummy geofence, restart the monitoring*/
+        if region.identifier == dummyRegionIdentifier {
+            refreshing = true
+            //locationManager.startUpdatingLocation()
+            log("Refreshing geofences due to dummy geofence exit")
+            //BEGIN REFRESH
+            var lat = location.latitude
+            var lon = location.longitude
+            log("New location = \(lat), \(lon)")
+            monitorNearbyRegions(lat, lon: lon)
+            //END REFRESH
+            /*var notification = UILocalNotification()
+            notification.timeZone = NSTimeZone.defaultTimeZone()
+            var dateTime = NSDate()
+            notification.fireDate = dateTime
+            notification.alertBody = "Exit dummy geofence. Refreshing geofences..."
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)*/
+        }
+        else {
+            handleTransition(region, transitionType: "exit", location: location)
+        }
+        /*}
+        else{
+        log("This event is not reliable. Ignore it.")
+        }*/    }
 
     func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
         let lat = (region as! CLCircularRegion).center.latitude
@@ -361,10 +514,11 @@ class GeoNotificationManager : NSObject, CLLocationManagerDelegate {
         log("Monitoring region " + region!.identifier + " failed " + error.description)
     }
 
-    func handleTransition(region: CLRegion!) {
+    func handleTransition(region: CLRegion!,transitionType: String!,location: CLLocationCoordinate2D!) {
         if let geo = store.findById(region.identifier) {
-            if let notification = geo["notification"].asDictionary {
-                notifyAbout(geo)
+            if geo["notification"].isDictionary {
+//                notifyAbout(geo)
+                notifyServer(geo,transitionType: transitionType, location: location)
             }
 
             NSNotificationCenter.defaultCenter().postNotificationName("handleTransition", object: geo.description)
@@ -384,6 +538,26 @@ class GeoNotificationManager : NSObject, CLLocationManagerDelegate {
         }
         UIApplication.sharedApplication().scheduleLocalNotification(notification)
 
+        if let vibrate = geo["notification"]["vibrate"].asArray {
+            if (!vibrate.isEmpty && vibrate[0].asInt > 0) {
+                AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+            }
+        }
+    }
+    
+    func notifyServer(geo: JSON,transitionType: String!,location: CLLocationCoordinate2D!) {
+        log("Creating notification")
+        let notification = UILocalNotification()
+        notification.timeZone = NSTimeZone.defaultTimeZone()
+        let dateTime = NSDate()
+        notification.fireDate = dateTime
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.alertBody = transitionType + " " + geo["notification"]["title"].asString!
+        if let json = geo["notification"]["data"] as? JSON {
+            notification.userInfo = ["geofence.notification.data": json.description]
+        }
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        
         if let vibrate = geo["notification"]["vibrate"].asArray {
             if (!vibrate.isEmpty && vibrate[0].asInt > 0) {
                 AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
